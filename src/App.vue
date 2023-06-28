7b8aaa0ffa495f30ab7e36f2c57b3cfbc088fe72
cloudhuh <claudiu.huhulia@gmail.com>
2023-06-28 16:35:05 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import AppTitle from './components/AppTitle.vue';
+import AppHero from './components/AppHero.vue';
+export default {
+  data() {
+    return {
+
+    }
+  },
+  components: {
+    AppTitle,
+    AppHero
+  }
+}
+
+</script>
+
+<template>
+  <AppTitle />
+</template>
+
+<style></style>
